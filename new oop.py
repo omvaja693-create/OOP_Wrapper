5103e4946a22dd75b5105c2cbab50bfa17b9843a
Om Vaja <omvaja693.com>
2026-02-12 16:32:42 +0000

    Fresh Uploads

--- new oop.py
+++ new oop.py
@@ -1,47 +1,50 @@
-#============================================================class===============================================================
+
+#============================================================class========================================================================================================================
 class Employee:
-    def __init__(self,emp_id, name, age, salary):
+    def __init__(self, name, age, emp_id, salary):
         self.__employee_id = emp_id
         self.__name = name
         self.__age = age
         self.__salary = salary
 
-    def getdata(self):
-        print(f"Employee`s id:{self.__employee_id} | Employee`s Name: {self.__name} | Employee`s Age: {self.__age} | Employee`s: {self.__salary}")
+    def Emp(self):
+        print(f"Employee`s id:{self.__employee_id} | Employee`s Name: {self.__name} | Employee`s Age: {self.__age} | Employee`s Salary: {self.__salary}")
+
 
 class Manager(Employee):
-    def __init__(self, emp_id, name, age, salary, department):
-        self.__employee_id = emp_id
-        self.__name = name
-        self.__age = age
-        self.__salary = salary
+    def __init__(self, name, age, emp_id, salary, dept):
+        super().__init__(name, age, emp_id, salary)   
+        self.__Department = dept
 
-        self.__Department = department
+    def Man(self):
+        super().Emp()   
+        print(f"Employee`s Dept: {self.__Department}")
 
-    def getdata(self):
-        print(f"Employee`s id:{self.__employee_id} | Employee`s Name: {self.__name} | Employee`s Age: {self.__age} | Employee`s: {self.__salary} | Employee`s Department: {self.Department}")
 
 class Developer(Employee):
-    def __init__(self):
-        self.__employee_id = int(input('Enter Employee`s id: '))
-        self.__name = input('Enter Employee`s Name: ')
-        self.__age = int(input('Enter Employee`s Age: '))
-        self.__salary = int(input('Enter Employee`s Salary: '))
-        self.__Developer = input('Enter Employee`s Language: ')
+    def __init__(self, name, age, emp_id, salary, lang):
+        super().__init__(name, age, emp_id, salary)   
+        self.__language = lang
 
-      def getdata(self):
-        print(f"Employee`s id:{self.__employee_id} | Employee`s Name: {self.__name} | Employee`s Age: {self.__age} | Employee`s: {self.__salary} | Employee`s Language: {}
+    def Dev(self):
+        super().Emp()   
+        print(f"Employee`s Language: {self.__language}")
 
-#==========================================================program===============================================================
+
+#==========================================================program=======================================================================================================================
 
 print('--- Python OOP Project: Employee Management System ---')
+
+employee = None
+manager = None
+developer = None
 
 while True:
     print()
     print('Choose an operation:')
     print('1. Create a Person')
-    print('2. Create an Employee:)
-    print('3. Create a Manager')
+    print('2. Create a Manager')
+    print('3. Create a Developer')
     print('4. Show Details')
     print('5. Exit')
     print()
@@ -50,17 +53,78 @@ while True:
     print()
 
     match choise:
-          case 1:
-              name = input('Enter Employee`s name: ')
-              age = int(input('Enter Employee`s Age: '))
-              emp_id = input("Enter Employee ID: ")
-              salary = int(input('Enter Employee`s Salary: '))
-        
-              employee = Employee(emp_id, name, age, salary)
-              print(f"Employee Name {name} Created Sucessfully...")
-          case 2:
-          case 3:pass
-          case 4:pass
-          case 5:break
-          case _:
+
+        case 1:
+            name = input('Enter Employee`s name: ')
+            age = int(input('Enter Employee`s Age: '))
+            emp_id = input("Enter Employee ID: ")
+            salary = int(input('Enter Employee`s Salary: '))
+
+            employee = Employee(name, age, emp_id, salary)
+            print(f"Person Name {name} Created Successfully...")
+
+        case 2:
+            name = input('Enter Employee`s name: ')
+            age = int(input('Enter Employee`s Age: '))
+            emp_id = input("Enter Employee ID: ")
+            salary = int(input('Enter Employee`s Salary: '))
+            dept = input("Enter Department: ")
+
+            manager = Manager(name, age, emp_id, salary, dept)
+            print(f"Manager Name {name} Created Successfully...")
+
+        case 3:
+            name = input('Enter Employee`s name: ')
+            age = int(input('Enter Employee`s Age: '))
+            emp_id = input("Enter Employee ID: ")
+            salary = int(input('Enter Employee`s Salary: '))
+            lang = input("Enter Programming Language: ")
+
+            developer = Developer(name, age, emp_id, salary, lang)
+            print(f"Developer Name {name} Created Successfully...")
+
+        case 4:
+            print("Choose Detail To Show...")
+            print("1. Person")
+            print("2. Manager")
+            print("3. Developer")
+
+            choise = int(input("Enter your choise: "))
+
+            match choise:
+                case 1:
+                    if employee:
+                        employee.Emp()
+                    else:
+                        print("No Person Created Yet.")
+
+                case 2:
+                    if manager:
+                        manager.Man()
+                    else:
+                        print("No Manager Created Yet.")
+
+                case 3:
+                    if developer:
+                        developer.Dev()
+                    else:
+                        print("No Developer Created Yet.")
+
+        case 5:
+            print("Exiting the system. Goodbye!")
+            break
+
+        case _:
+            print("Invalid Choice...")
+
+
+
+
+
+
+
+
+
+
+            
 
